6faea484884a47340439f36563910b1d1225ebcf
AtharvBhavsar <atharvbhavsars@gmail.com>
2026-01-10 12:20:06 +0530

    commit check on main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import styles from "./styles/App.module.css";
 
 function App() {
-  return (
-    <>
-      <div className={styles.style}></div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
